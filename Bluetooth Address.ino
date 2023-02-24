a41650bcf1bc6b52456ca9d039bfeedce3c730db
Jon Reginald Reyrao <35319230+Rowataro@users.noreply.github.com>
2023-02-24 00:46:10 -0800

    Add files via upload

---
+++ Bluetooth Address.ino
@@ -0,0 +1,50 @@
+#include "esp_bt_main.h"
+#include "esp_bt_device.h"
+ 
+//printDeviceAddress prints "C8:F0:9E:9C:87:52" for ESP32
+
+bool initBluetooth()
+{
+  if (!btStart()) {
+    Serial.println("Failed to initialize controller");
+    return false;
+  }
+ 
+  if (esp_bluedroid_init() != ESP_OK) {
+    Serial.println("Failed to initialize bluedroid");
+    return false;
+  }
+ 
+  if (esp_bluedroid_enable() != ESP_OK) {
+    Serial.println("Failed to enable bluedroid");
+    return false;
+  }
+ 
+}
+ 
+void printDeviceAddress() {
+ 
+  const uint8_t* point = esp_bt_dev_get_address();
+ 
+  for (int i = 0; i < 6; i++) {
+ 
+    char str[3];
+ 
+    sprintf(str, "%02X", (int)point[i]);
+    Serial.print(str);
+ 
+    if (i < 5){
+      Serial.print(":");
+    }
+ 
+  }
+}
+ 
+void setup() {
+  Serial.begin(115200);
+ 
+  initBluetooth();
+  printDeviceAddress();
+}
+ 
+void loop() {}
